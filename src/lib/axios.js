70ef9b429f9d7b43f7bc80c5d2157f33f3834be2
js3na <passosjoaogabriel29@gmail.com>
2024-12-04 14:36:48 -0300

    fix: csrf token

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
     withCredentials: true,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
